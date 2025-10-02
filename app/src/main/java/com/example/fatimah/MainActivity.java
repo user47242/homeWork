6d6fd20d4850601b0e7f0a4deb2535a238e94ed9
ex12 <layanawad123123@gmail.com>
2025-10-02 10:30:14 +0300

    homeWork

--- app/src/main/java/com/example/fatimah/MainActivity.java
+++ app/src/main/java/com/example/fatimah/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.fatimah;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,42 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button up,down,left,right;
+    private ImageView img;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+       up = findViewById(R.id.bUp);
+        down = findViewById(R.id.bDown);
+        left = findViewById(R.id.bLeft);
+        right = findViewById(R.id.bRight);
+        img = findViewById(R.id.imageView);
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img.setY(img.getY()-5);
+            }
         });
+        down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img.setY(img.getY()+5);
+            }
+        });
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img.setX(img.getX()-5);
+            }
+        });
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img.setX(img.getX()+5);
+            }
+        });
+
+        };
     }
-}
